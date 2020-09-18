c87b6f050ce5e7746c5a2a7a42ee27f3b977c72b
zarasimasu <ouz4r83y325332g@gmail.com>
2020-09-18 17:51:34 +0900

    商品出品機能のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品情報入力' do
+    context '商品出品情報入力がうまくいくとき' do
+      it "すべての項目が存在すれば商品情報が保存できること" do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '商品情報入力がうまくいかないとき' do
+      it "商品名が空だと保存できない" do
+        @item.name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "商品の説明が空だと保存できない" do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it "カテゴリーが--だと保存できない" do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it "商品の状態が--だと保存できない" do
+        @item.status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status must be other than 1")
+      end
+      it "配送料の負担が--だと保存できない" do
+        @item.cost_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Cost must be other than 1")
+      end
+      it "発送元の地域が--だと保存できない" do
+        @item.area_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Area must be other than 1")
+      end
+      it "発送までの日数が--だと保存できない" do
+        @item.days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days must be other than 1")
+      end
+      it "価格が空だと保存できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number", "Price is not a number")
+      end
+      it "価格が300以下だと保存できない" do
+        @item.price = 200
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it "価格が9999999以上だと保存できない" do
+        @item.price = 99999999
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+    end
+  end
 end
