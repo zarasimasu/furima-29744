b8d149a46a40494bdce33e815212d0ae816f627f
zarasimasu <ouz4r83y325332g@gmail.com>
2020-09-23 16:14:39 +0900

    ユーザーの状態による画面遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,12 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    elsif user_signed_in?
+    else
+      redirect_to new_user_session_path
+    end
   end
 
 
